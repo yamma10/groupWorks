e1b2a485a7e4a68a89beb86cf7bc423d61bbb4a9
yamma10 <yamtt1326@gmail.com>
2023-12-18 16:05:30 +0900

    dbとの接続に成功

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import express from 'express';
 import usersRoute  from './src/routes/users';
+import { PrismaClient } from "@prisma/client";
+import mssql from "mssql";
+import {config} from "./config";
+import fs from "fs";
+// const prisma = new PrismaClient();
 
 const app: express.Express = express();
 app.use(express.json());
@@ -13,4 +18,20 @@ app.use("/users", usersRoute);
 
 app.get("/", (req: express.Request, res: express.Response) => {
     res.send("hello world")
+})
+
+app.get("/test", async(req: express.Request,res: express.Response) => {
+    try {
+        const conn = await mssql.connect(config);
+
+        const str = fs.readFileSync("./src/sql/test.sql", "utf-8")
+
+        const res = await conn.request().query(str)
+        console.log(res)
+        
+    } catch(e: any) {
+        res.status(500).send(e.message)
+    }
+
+    res.end();
 })
