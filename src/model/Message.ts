8f6c104b37280e21f5cc93bcd63f81305ea510ea
yamma10 <yamtt1326@gmail.com>
2024-01-07 15:05:05 +0900

    RoomAPIの変更

--- src/model/Message.ts
+++ src/model/Message.ts
@@ -1,13 +1,15 @@
 
 export class Message {
-    constructor(roomId: number, employeeCode: number, text: string, date: Date) {
+    constructor(roomId: number, employeeCode: number, employeeName: string,  text: string, date: Date) {
         this.roomId = roomId;
         this.employeeCode = employeeCode;
+        this.employeeName = employeeName;
         this.text = text;
         this.date = date;
     }
     roomId: number;
     employeeCode: number;
+    employeeName: string;
     text: string;
     date: Date;
 }
